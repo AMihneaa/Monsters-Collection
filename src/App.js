210090ae57615ea3cef12efd99907c185f269396
Mihnea <asmihnea5@gmail.com>
2023-03-19 02:51:40 +0200

    Invatat mai bine despre Life Cycling Methods in React Class Components, precum componentDidMount() si despre ordinea executarii metodelor in React ( Constructors, Render, componentDidMount ). Cu aceste informatii, am reusit sa implementez logica din spatele unui search input care filtreaza array-ul dupa valoarea din input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+
+      URL: "https://jsonplaceholder.typicode.com/users",
+
+      searchFilter: " ",
+    };
+  }
+
+  componentDidMount() {
+    fetch(this.state.URL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            // console.log(this.state.monsters);
+          }
+        );
+      });
+  }
+
+  render() {
+    const filterMonster = this.state.monsters.filter((monster) =>
+      monster.name.toLocaleLowerCase().includes(this.state.searchFilter)
+    );
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type={"search"}
+          placeholder={"Search monsters"}
+          onChange={(ev) => {
+            const searchFilter = ev.target.value.toLocaleLowerCase();
+
+            this.setState(() => {
+              return { searchFilter };
+            });
+          }}
+        />
+        {filterMonster.map((monsterElem) => {
+          return (
+            <div key={monsterElem.id}>
+              <h1>{monsterElem.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
